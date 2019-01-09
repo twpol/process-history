79e1bb86d6b1cd835b2a475df16d5934af081436
James Ross <git@james-ross.co.uk>
2019-01-09 21:21:25 +0000

    feat: Add empty app and base config

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Process_History
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
